87467f90e1d07d4e9115e75ea6069ba682b9e6ce
hafidz <hafidzzpermana09@gmail.com>
2018-03-11 07:51:18 +0700

    exported components

---
+++ src/components/InputSection/index.js
@@ -0,0 +1,11 @@
+// export FilterBrands from './FilterBrands';
+// export FilterYears from './FilterYears';
+// export SelectBrandsBox from './SelectBrandsBox';
+// export SelectYearsBox from './SelectYearsBox';
+// export SearchBar from './SearchBar';
+
+export { FilterBrands } from './FilterBrands';
+export { FilterYears } from './FilterYears';
+export { SelectBrandsBox } from './SelectBrandsBox';
+export { SelectYearsBox } from './SelectYearsBox';
+export { SearchBar } from './SearchBar';
